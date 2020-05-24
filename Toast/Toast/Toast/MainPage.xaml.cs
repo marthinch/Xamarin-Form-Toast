c93e9200aace19b0f9f171ed7e13d1fa96c62711
Marthin CH <itdev2@tripcetera.com>
2020-05-24 20:58:44 +0700

    INITIALIZE PROJECT & GIT IGNORE

---
+++ Toast/Toast/Toast/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.Toasts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Toast
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void ButtonShowToast_Clicked(object sender, EventArgs e)
+        {
+            var notificator = DependencyService.Get<IToastNotificator>();
+
+            var options = new NotificationOptions()
+            {
+                Title = "Title",
+                Description = "Description"
+            };
+
+            var result = await notificator.Notify(options);
+        }
+    }
+}
